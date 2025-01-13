0d75f88b322f3b23367c55a8ff6e9e29f33cdb6b
Edgar Juarez <2523360010emjuarezt@gmail.com>
2025-01-13 13:44:24 -0600

    v1.3

--- node-1/app.js
+++ node-1/app.js
@@ -1,1 +1,24 @@
-hola mundo
+const fs = require('fs')
+
+/* fs.readFile('ejemplo.txt', 'utf-8', (e,data)=>{
+    if (e) {
+        console.log(e);
+        return;        
+        
+    }
+    console.log(data);
+    
+
+}) 
+
+const content = 'Hola'
+fs.writeFile('file.txt', content, (e)=>{
+    if (e) {
+        console.log(e);
+        return;        
+        
+    }
+    console.log('Archivo lleno exitosamente');
+    
+
+})*/
