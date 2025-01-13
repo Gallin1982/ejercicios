c3d6e715b552aacc7c2979222eff3de8f9ef7633
Edgar Juarez <2523360010emjuarezt@gmail.com>
2025-01-13 13:51:36 -0600

    v1.4

--- node-1/app.js
+++ node-1/app.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const http = require('http')
 
 /* fs.readFile('ejemplo.txt', 'utf-8', (e,data)=>{
     if (e) {
@@ -21,4 +22,16 @@ fs.writeFile('file.txt', content, (e)=>{
     console.log('Archivo lleno exitosamente');
     
 
+})
+
+const server = http.createServer((req, res)=> {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World')
+
+})
+
+server.listen(3000, ()=>{
+    console.log("Servidor corriendo en el puerto 3000");
+    
 })*/
