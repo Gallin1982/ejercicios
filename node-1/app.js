ac106aed28c26424aefc44a168af3d16745cafdb
Edgar Juarez <2523360010emjuarezt@gmail.com>
2025-01-13 14:12:26 -0600

    V1.5

--- node-1/app.js
+++ node-1/app.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 const http = require('http')
+const path = require('path')
+const os = require('os')
+const url = require('url')
 
 /* fs.readFile('ejemplo.txt', 'utf-8', (e,data)=>{
     if (e) {
@@ -34,4 +37,21 @@ const server = http.createServer((req, res)=> {
 server.listen(3000, ()=>{
     console.log("Servidor corriendo en el puerto 3000");
     
-})*/
+})
+
+const directory = 'user/local/'
+const fileName = 'ejemplo.txt'
+const fullPath = path.join(directory,fileName)
+console.log(fullPath);
+
+
+console.log('Plataforma', os.platform());
+console.log('CPU Arquitectura:', os.arch());
+console.log('Memoria:', os.totalmem());
+console.log('Memoria libre', os.freemem() );*/
+
+/* const miUrl = new URL('https://ejemplo.com:8080/path/nombre?query=hola#hash')
+console.log('Host', miUrl.host);
+console.log('Nombre de la ruta', miUrl.pathname);
+console.log('Parametros', miUrl.searchParams.get('query')); */
+
